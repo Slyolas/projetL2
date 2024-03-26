63a42e08f7ba67602ea1cf77b8078984b76ed74e
Bossard Guilian <s2200833@ic2s120-15.univ-lemans.fr>
2024-03-26 15:16:30 +0100

    Mise en commun dossiers Nathan et Guilian dans Programme

--- Nathan/Code/FichierH/fonctionCommun.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
-
-void erreur(const char *message);
-void redimensionnement_fenetre(SDL_Event event, int *largeur, int *hauteur);
-int clic_plein_ecran(SDL_Event event, SDL_Rect *rectangle_plein_ecran, SDL_bool *plein_ecran, SDL_Window **window);
