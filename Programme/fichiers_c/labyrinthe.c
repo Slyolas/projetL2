30a95f86584ea055fa0e82fb42b7ebc3c66fcdbd
Slyolas <157492823+Slyolas@users.noreply.github.com>
2024-04-07 12:04:53 +0200

    Ajout de commentaire

--- Programme/fichiers_c/labyrinthe.c
+++ Programme/fichiers_c/labyrinthe.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <string.h>
 
-// Define window dimensions
+// Dimensions de la fenêtre
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
-// Define tile dimensions
+// Dimensions des tiles
 #define TILE_SIZE_X SCREEN_WIDTH / 32
 #define TILE_SIZE_Y SCREEN_HEIGHT / 24
 
@@ -16,7 +16,7 @@
 #define LEFT 2
 #define RIGHT 3
 
-// Function to load an image into a texture
+// Fonction pour charger une image dans une texture
 SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* filePath) {
     SDL_Surface* surface = IMG_Load(filePath);
     if (!surface) {
@@ -28,14 +28,14 @@ SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* filePath) {
     return texture;
 }
 
-// Function to process user commands
-void processCommand(int* playerX, int* playerY, int* blockX, int* blockY, int tilemap[24][32], int direction) {
+// Fonction pour traiter les commandes utilisateur
+void traitementTouches(int* playerX, int* playerY, int* blockX, int* blockY, int tilemap[24][32], int direction) {
 
-    // Check if the player is next to the block
-    if ((*playerX == *blockX - 1 && *playerY == *blockY) ||   // Player is to the left of the block
-        (*playerX == *blockX + 1 && *playerY == *blockY) ||   // Player is to the right of the block
-        (*playerX == *blockX && *playerY == *blockY - 1) ||   // Player is above the block
-        (*playerX == *blockX && *playerY == *blockY + 1)) {   // Player is below the block
+    // Vérification si le joueur est à côté du bloc
+    if ((*playerX == *blockX - 1 && *playerY == *blockY) ||   // Le joueur est à gauche du bloc
+        (*playerX == *blockX + 1 && *playerY == *blockY) ||   // Le joueur est à droite du bloc
+        (*playerX == *blockX && *playerY == *blockY - 1) ||   // Le joueur est au dessus du bloc
+        (*playerX == *blockX && *playerY == *blockY + 1)) {   // Le joueur est en dessous du bloc
 
         // Le personnage est au dessus du bloc
         if (*playerX == *blockX && *playerY == *blockY - 1) { 
@@ -124,25 +124,31 @@ void processCommand(int* playerX, int* playerY, int* blockX, int* blockY, int ti
     }
 }
 
-// Function to update border tiles when the block reaches the end of the labyrinth
+// Fonction pour mettre à jour les tuiles de bordure lorsque le bloc atteint la fin du labyrinthe
 int updateBorderTiles(SDL_Renderer* renderer, SDL_Texture* finishTexture, int tilemap[24][32], int tileX, int tileY) {
 
+    /* Mise à jour du rendu de la tuile courante */
     tilemap[tileY][tileX] = 3;
     SDL_Rect dstRect = {tileX * TILE_SIZE_X, tileY * TILE_SIZE_Y, TILE_SIZE_X, TILE_SIZE_Y};
     SDL_RenderCopy(renderer, finishTexture, NULL, &dstRect);
     SDL_RenderPresent(renderer);
     SDL_Delay(20);
 
-	
+	/* Vérification des tuiles adjacentes pour permettre un appel récursif et ainsi changé toute les tuiles de la bordure */
+    
+    /* Tuile à droite de la courante */
     if(tilemap[tileY][tileX + 1] == 2 && tileX < 31) {
         return updateBorderTiles(renderer, finishTexture, tilemap, tileX + 1, tileY);
     }
+    /* Tuile à gauche de la courante */
     else if(tilemap[tileY][tileX - 1] == 2 && tileX > 0){
         return updateBorderTiles(renderer, finishTexture, tilemap, tileX - 1, tileY);
     }
+    /* Tuile en dessous de la courante */
     else if(tilemap[tileY + 1][tileX] == 2 && tileY < 31){
         return updateBorderTiles(renderer, finishTexture, tilemap, tileX, tileY + 1);
     }
+    /* Tuile au dessus de la courante */
     else if(tilemap[tileY - 1][tileX] == 2 && tileY > 0){
         return updateBorderTiles(renderer, finishTexture, tilemap, tileX, tileY - 1);
     }
@@ -151,6 +157,8 @@ int updateBorderTiles(SDL_Renderer* renderer, SDL_Texture* finishTexture, int ti
 }
 
 int main() {
+
+    // Déclaration
     SDL_Window* window;
     SDL_Renderer* renderer;
 
@@ -162,6 +170,9 @@ int main() {
     SDL_Texture* finishTexture;
 
     SDL_Event event;
+
+    // Initialisation de la carte du labyrinthe
+    // 0 : sol, 1 : mur, 2 : bordure, 3 : tuile de fin
     int tilemap[24][32] = { /* 24*32 max avec ces dimensions */
         {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
         {2, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 2},
@@ -201,13 +212,13 @@ int main() {
 
     int quit = 0;
 
-    // Initialize SDL
+    // Initialisation SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("Failed to initialize SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Create window
+    // Création de la fenêtre
     window = SDL_CreateWindow("Tilemap Labyrinth", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     if (!window) {
         printf("Failed to create window: %s\n", SDL_GetError());
@@ -215,7 +226,7 @@ int main() {
         return 1;
     }
 
-    // Create renderer
+    // Création du renderer
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!renderer) {
         printf("Failed to create renderer: %s\n", SDL_GetError());
@@ -224,7 +235,7 @@ int main() {
         return 1;
     }
 
-    // Load tile textures
+    // Chargement des textures des tuiles
     wallTexture = loadTexture(renderer, "../images/mur.png");
     floorTexture = loadTexture(renderer, "../images/sol.png");
     borderTexture = loadTexture(renderer, "../images/obsi.png");
@@ -238,14 +249,20 @@ int main() {
         return 1;
     }
 
-    // Main loop
+    // Boucle principale
     while (!quit) {
-        // Event handling
+        // Gestion des événements
         while (SDL_PollEvent(&event) != 0) {
             switch(event.type){
                 case SDL_QUIT:
                     quit = 1;
                     break;
+                
+                /* 
+                    On peut pas traiter l'appui de 2 touches en même temps donc 
+                    On stocke une valeur pour permettre l'appuie de 2 touches en même 
+                    Et ainsi pouvoir traiter cette information 
+                */
                 case SDL_KEYDOWN:
                     if(event.key.keysym.sym == SDLK_UP){
                         up = 1;
@@ -286,9 +303,10 @@ int main() {
                     break;
             }
         }
+        /* Traitement de l'appuie des touches */
         if(up){
             if(e){
-                processCommand(&playerX, &playerY, &blockX, &blockY, tilemap, UP);
+                traitementTouches(&playerX, &playerY, &blockX, &blockY, tilemap, UP);
             }
             // On vérifie que c'est bien un chemin et qu'il n'y a pas le bloc
             else if (tilemap[playerY - 1][playerX] == 0 || tilemap[playerY - 1][playerX] == 3 && !(playerX == blockX && playerY == blockY + 1)) {
@@ -298,7 +316,7 @@ int main() {
         }
         if(down){
             if(e){
-                processCommand(&playerX, &playerY, &blockX, &blockY, tilemap, DOWN);
+                traitementTouches(&playerX, &playerY, &blockX, &blockY, tilemap, DOWN);
             }
             // On vérifie que c'est bien un chemin et qu'il n'y a pas le bloc
             else if (tilemap[playerY + 1][playerX] == 0 || tilemap[playerY + 1][playerX] == 3 && !(playerX == blockX && playerY == blockY - 1)) {
@@ -308,7 +326,7 @@ int main() {
         }
         if(left){
             if(e){
-                processCommand(&playerX, &playerY, &blockX, &blockY, tilemap, LEFT);
+                traitementTouches(&playerX, &playerY, &blockX, &blockY, tilemap, LEFT);
             }
             // On vérifie que c'est bien un chemin et qu'il n'y a pas le bloc
             else if (tilemap[playerY][playerX - 1] == 0 || tilemap[playerY][playerX - 1] == 3 && !(playerX == blockX + 1 && playerY == blockY)) {
@@ -318,7 +336,7 @@ int main() {
         }
         if(right){
             if(e){
-                processCommand(&playerX, &playerY, &blockX, &blockY, tilemap, RIGHT);
+                traitementTouches(&playerX, &playerY, &blockX, &blockY, tilemap, RIGHT);
             }
             // On vérifie que c'est bien un chemin et qu'il n'y a pas le bloc
             else if (tilemap[playerY][playerX + 1] == 0 || tilemap[playerY][playerX + 1] == 3 && !(playerX == blockX - 1 && playerY == blockY)) {
@@ -327,13 +345,14 @@ int main() {
             }
         }
 
-        // Clear renderer
+        // Nettoyer le renderer
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Render tilemap
+        // Rendu tilemap
         for (int y = 0; y < 24; ++y) {
             for (int x = 0; x < 32; ++x) {
+                // Rendu de chaque tuile en fonction de son type
                 int tileType = tilemap[y][x];
                 SDL_Texture* texture = NULL;
                 if(tileType == 1){
@@ -355,27 +374,27 @@ int main() {
             }
         }
 
-        // Render block
+        // Rendu du bloc
         SDL_Rect blockRect = {blockX * TILE_SIZE_X, blockY * TILE_SIZE_Y, TILE_SIZE_X, TILE_SIZE_Y};
         SDL_RenderCopy(renderer, blockTexture, NULL, &blockRect);
 
-        // Render player
+        // Rendu du joueur
         SDL_Rect playerRect = {playerX * TILE_SIZE_X, playerY * TILE_SIZE_Y, TILE_SIZE_X, TILE_SIZE_Y};
         SDL_RenderCopy(renderer, playerTexture, NULL, &playerRect);
 
 
-        // Check if the block reaches the end of the labyrinth
+        // Vérifier si le bloc atteint la fin du labyrinthe et mettre à jour les tuiles de bordure si nécessaire
         if(blockX == 30 && blockY == 22){
             updateBorderTiles(renderer, finishTexture, tilemap, 31, 23);
             printf("Oui\n");
             quit = 1;
         }
 
-        // Render to screen
+        // Actualiser le renderer
         SDL_RenderPresent(renderer);
     }
 
-    // Clean up
+    // Nettoyer et quitter
     SDL_DestroyTexture(wallTexture);
     SDL_DestroyTexture(floorTexture);
     SDL_DestroyTexture(borderTexture);
