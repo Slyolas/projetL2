cf2846feba54c941a551a26fd0e2b31716f24ae0
Perron Nathan <s2200467@ic2s130-13.univ-lemans.fr>
2024-04-12 11:18:24 +0200

    Changement nom variables + commentaires (loading)

--- Programme/fichiers_c/loading.c
+++ Programme/fichiers_c/loading.c
@@ -4,37 +4,37 @@
 #include <stdbool.h>
 #include <time.h>
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const int BAR_WIDTH = 400;
-const int BAR_HEIGHT = 30;
-const int SPIN_RADIUS = 20;
+const int LARGEUR = 800;
+const int HAUTEUR = 600;
+const int LARGEUR_BARRE = 400;
+const int HAUTEUR_BARRE = 30;
 
-void drawLoadingScreen(SDL_Renderer* renderer, int percentage, TTF_Font* font) {
-    // Clear the screen
+void rendu_ecran_chargement(SDL_Renderer* renderer, int pourcentage, TTF_Font* font) {
+
+    /* Nettoyer le rendu */
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    // Draw the loading bar
-    SDL_Rect loadingBarRect = { (SCREEN_WIDTH - BAR_WIDTH) / 2, SCREEN_HEIGHT / 2 - BAR_HEIGHT / 2, BAR_WIDTH, BAR_HEIGHT };
+    /* Barre de chargement */
+    SDL_Rect rectangle_barre_chargement = { (LARGEUR - LARGEUR_BARRE) / 2, HAUTEUR / 2 - HAUTEUR_BARRE / 2, LARGEUR_BARRE, HAUTEUR_BARRE };
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &loadingBarRect);
+    SDL_Renderrectangle_remplissage(renderer, &rectangle_barre_chargement);
 
-    int fillWidth = BAR_WIDTH * percentage / 100;
-    SDL_Rect fillRect = { (SCREEN_WIDTH - BAR_WIDTH) / 2, SCREEN_HEIGHT / 2 - BAR_HEIGHT / 2, fillWidth, BAR_HEIGHT };
+    int remplissage_largeur = LARGEUR_BARRE * pourcentage / 100;
+    SDL_Rect rectangle_remplissage = { (LARGEUR - LARGEUR_BARRE) / 2, HAUTEUR / 2 - HAUTEUR_BARRE / 2, remplissage_largeur, HAUTEUR_BARRE };
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-    SDL_RenderFillRect(renderer, &fillRect);
+    SDL_Renderrectangle_remplissage(renderer, &rectangle_remplissage);
 
-    // Draw the percentage text
-    SDL_Color textColor = { 255, 255, 255, 255 };
+    /* Texte du pourcentage */
+    SDL_Color couleur_police = { 255, 255, 255, 255 };
     char text[20];
-    sprintf(text, "Loading... %d%%", percentage);
-    SDL_Surface* textSurface = TTF_RenderText_Solid(font, text, textColor);
-    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-    SDL_Rect textRect = { (SCREEN_WIDTH - textSurface->w) / 2, SCREEN_HEIGHT / 2 - textSurface->h - 20, textSurface->w, textSurface->h };
-    SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
-    SDL_FreeSurface(textSurface);
-    SDL_DestroyTexture(textTexture);
+    sprintf(text, "Loading... %d%%", pourcentage);
+    SDL_Surface* surface_texte = TTF_RenderText_Solid(font, text, couleur_police);
+    SDL_Texture* texture_texte = SDL_CreateTextureFromSurface(renderer, surface_texte);
+    SDL_Rect rectangle_texte = { (LARGEUR - surface_texte->w) / 2, HAUTEUR / 2 - surface_texte->h - 20, surface_texte->w, surface_texte->h };
+    SDL_RenderCopy(renderer, texture_texte, NULL, &rectangle_texte);
+    SDL_FreeSurface(surface_texte);
+    SDL_DestroyTexture(texture_texte);
 
 }
 
@@ -43,33 +43,33 @@ int main(int argc, char* argv[]) {
     SDL_Renderer* renderer;
     TTF_Font* font;
     
-    // Initialize SDL
+    /* Initialisation de SDL */
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Create window
-    window = SDL_CreateWindow("Loading Screen", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    /* Initialisation de la fenetre */
+    window = SDL_CreateWindow("Loading Screen", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, LARGEUR, HAUTEUR, SDL_WINDOW_SHOWN);
     if (window == NULL) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Create renderer
+    /* Initialisation du rendu */
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == NULL) {
         printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Initialize SDL_ttf
+    /* Initialisation de la bibliothèque SDL_ttf */
     if (TTF_Init() == -1) {
         printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
         return 1;
     }
 
-    // Load font
+    /* Chargement de la police */
     font = TTF_OpenFont("../polices/02587_ARIALMT.ttf", 24);
     if (font == NULL) {
         printf("Failed to load font! SDL_ttf Error: %s\n", TTF_GetError());
@@ -77,7 +77,7 @@ int main(int argc, char* argv[]) {
     }
 
     int quit = 1;
-    int percentage = 0;
+    int pourcentage = 0;
 
     while (quit) {
         SDL_Event e;
@@ -89,23 +89,23 @@ int main(int argc, char* argv[]) {
 
         srand(time(NULL));
 
-        // Simulate loading progress
-        percentage += rand() % 3;
-        if (percentage > 100) {
-            percentage = 0;
+        /* Simulation de la progression du chargement */
+        pourcentage += rand() % 3;
+        if (pourcentage > 100) {
+            pourcentage = 0;
         }
 
-        // Render loading screen
-        drawLoadingScreen(renderer, percentage, font);
+        /* Rendu */
+        rendu_ecran_chargement(renderer, pourcentage, font);
 
-        // Update screen
+        /* Mise à jour de l'écran */
         SDL_RenderPresent(renderer);
 
-        // Add delay to simulate loading
+        /* Ajout d'un delai pour simuler le chargement */
         SDL_Delay(50);
     }
 
-    // Cleanup
+    /* Nettoyage et Quitter */
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
